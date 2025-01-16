b2b89514c9d8fbbc823ac46139a7a446ac909ae6
khup practice <khup.practice@gmail.com>
2025-01-16 17:18:18 +0530

    IMapper created which help to map model and DTO data There need to install two packages: 1) AutoMapper 2) AutoMapper.extension.Tools

---
+++ userInformation_Angular_dotnet/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using userInformation_Angular_dotnet.Models;
+using userInformation_Angular_dotnet.Models.DTO;
+
+namespace userInformation_Angular_dotnet
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig() 
+        {
+            CreateMap<Registration, RegistrationRequestDTO>().ReverseMap();
+        }
+    }
+}
--- userInformation_Angular_dotnet/Program.cs
+++ userInformation_Angular_dotnet/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using userInformation_Angular_dotnet;
+using userInformation_Angular_dotnet.Repository;
+using userInformation_Angular_dotnet.Repository.IRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
+
+// Injecting reposistory here.
+builder.Services.AddHttpClient<IRegistration, RegistartionRepo>();
+builder.Services.AddScoped<IRegistration, RegistartionRepo>();
+
+// Injecting AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
