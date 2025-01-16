e8c26619841c5b36832a6b1d75c08331d5235272
khup practice <khup.practice@gmail.com>
2025-01-16 17:15:36 +0530

    Repository and IRepository created and logic added in it.

---
+++ userInformation_Angular_dotnet/Repository/IRepository/IRegistarion.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using userInformation_Angular_dotnet.Models;
+
+namespace userInformation_Angular_dotnet.Repository.IRepository
+{
+    public interface IRegistration
+    {
+        Task<List<Registration>> GetAllUserList(Expression<Func<Registration, bool>> filter = null);
+        Task CreateNewUser(Registration entity);
+        Task<Registration> GetUserById(Guid id);
+        Task DeleteUser(Guid id);
+        Task UpdateUser(Registration entity);
+        Task SaveAsync(Registration entity);
+    }
+}
---
+++ userInformation_Angular_dotnet/Repository/RegistartionRepo.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+using userInformation_Angular_dotnet.Models;
+using userInformation_Angular_dotnet.Repository.IRepository;
+
+namespace userInformation_Angular_dotnet.Repository
+{
+    public class RegistartionRepo : IRegistration
+    {
+        private readonly ApplicationDbContext _DbContext;
+
+        public RegistartionRepo(ApplicationDbContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+        public async Task CreateNewUser(Registration entity)
+        {
+            await _DbContext.UserListTable.AddAsync(entity);
+            await SaveAsync(entity);
+        }
+
+        public Task DeleteUser(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Registration>> GetAllUserList(Expression<Func<Registration, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Registration> GetUserById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SaveAsync(Registration entity)
+        {
+           await _DbContext.SaveChangesAsync();
+        }
+
+        public Task UpdateUser(Registration entit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
