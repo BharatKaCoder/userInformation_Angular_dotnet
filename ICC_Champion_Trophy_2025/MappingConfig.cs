c36403b739c201a45c31a14c7de4c52e31df0c43
khup practice <khup.practice@gmail.com>
2025-01-19 22:46:53 +0530

    AutoMapper installed and injected in program.cs file

---
+++ ICC_Champion_Trophy_2025/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ICC_Champion_Trophy_2025.Model;
+using ICC_Champion_Trophy_2025.Model.DTO;
+
+namespace ICC_Champion_Trophy_2025
+{
+    public class MappingConfig :Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Teams, TeamsDTO>().ReverseMap();
+        }
+    }
+}
--- ICC_Champion_Trophy_2025/Program.cs
+++ ICC_Champion_Trophy_2025/Program.cs
@@ -1,12 +1,23 @@
 using ICC_Champion_Trophy_2025;
+using ICC_Champion_Trophy_2025.Repository;
+using ICC_Champion_Trophy_2025.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// 1) DB context Injected
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
+
+// 2) Repository file injected
+builder.Services.AddHttpClient<ITeamRepository, TeamRepository>();
+builder.Services.AddScoped<ITeamRepository, TeamRepository>();
+
+// 3) AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
