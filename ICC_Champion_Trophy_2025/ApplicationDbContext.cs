c1a7154880601c49383920e12ba81b8e713729e5
khup practice <khup.practice@gmail.com>
2025-01-19 19:50:28 +0530

    Connection string added in appConfig & Db context created and Injected in program.cs

---
+++ ICC_Champion_Trophy_2025/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ICC_Champion_Trophy_2025.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ICC_Champion_Trophy_2025
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Teams> Teams { get; set; }
+        public DbSet<Players> Players { get; set; }
+        public DbSet<PlayerDetails>PlayerDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
